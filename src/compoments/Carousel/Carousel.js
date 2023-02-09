638b37d43c47c0d6b350f24a03d522b8efb08c3e
minhtrifit <minhtri.fit@gmail.com>
2023-02-09 19:25:13 +0700

    fix nav

--- src/compoments/Carousel/Carousel.js
+++ src/compoments/Carousel/Carousel.js
@@ -1,31 +1,53 @@
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import React from "react";
-import Slider from "react-slick";
+import { useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation, Autoplay } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 import "./Carousel.scss";
 
-function MyCarousel() {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+const MyCarousel = () => {
+  const progressCircle = useRef(null);
+  const progressContent = useRef(null);
+  const onAutoplayTimeLeft = (s, time, progress) => {
+    progressCircle.current.style.setProperty("--progress", 1 - progress);
+    progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
   };
-
   return (
-    <Slider {...settings} className="slider-container">
-      <div className="slider-box">
-        <img src="../../img/banner1.png" />
-      </div>
-      <div className="slider-box">
-        <img src="../../img/2.png" />
-      </div>
-      <div className="slider-box">
-        <img src="../../img/3.png" />
-      </div>
-    </Slider>
+    <>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        onAutoplayTimeLeft={onAutoplayTimeLeft}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="../../img/banner1.png" alt="banner1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../../img/banner1.png" alt="banner1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../../img/banner1.png" alt="banner1" />
+        </SwiperSlide>
+        <div className="autoplay-progress" slot="container-end">
+          <svg viewBox="0 0 48 48" ref={progressCircle}>
+            <circle cx="24" cy="24" r="20"></circle>
+          </svg>
+          <span ref={progressContent}></span>
+        </div>
+      </Swiper>
+    </>
   );
-}
+};
 
 export default MyCarousel;
